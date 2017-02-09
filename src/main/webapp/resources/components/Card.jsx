eb96ccf0eea2c2f6ccf5570e7369b0850b8f7001
Artem K <artem.karpov@yahoo.com>
2017-02-09 18:35:19 +0200

    Just works

--- src/main/webapp/resources/components/Card.jsx
+++ src/main/webapp/resources/components/Card.jsx
@@ -5,8 +5,10 @@ export default class Card extends React.Component{
     constructor(props) {
         super(props);
         this.state = {
-            name: "Noname",
-            avatar_url: "http://github-jobs.s3.amazonaws.com/aa333d4e-adeb-11e2-9d28-44d170059efd.png"
+            currency: "Noname",
+            price: "20000",
+            riskLvl: "RISKY",
+            avatar_url: "http://bitcoin.org/img/icons/opengraph.png"
         };
     }
 
@@ -16,16 +18,33 @@ export default class Card extends React.Component{
             url: 'http://api.github.com/users/' + this.props.currency,
             dataType: 'json',
             success: function(data){
-                component.setState(data);
+                console.log(data);
+                //component.setState(Object.assign({},data));
             }
         });
     }
 
     render() {
         return <div className="col-6 col-lg-4">
+            <div className="thumbnail">
                 <img src={this.state.avatar_url} width="80"/>
-                <h3>{this.state.name}</h3>
-                <button className="btn btn-secondary" href="#" role="button">Trade</button>
+                <div className="caption">
+                    <p>{this.state.currency}</p>
+                    <p>{this.state.price}</p>
+                    <p>{this.state.riskLvl}</p>
+                    <div className='progress'>
+                        <div className='progress-bar progress-bar-danger'
+                             role='progressbar'
+                             aria-valuenow='70'
+                             aria-valuemin='0'
+                             aria-valuemax='100'
+                             style={{width: '70%'}}>
+                            <span className='sr-only'>70% Complete</span>
+                        </div>
+                    </div>
+                </div>
+                <button className="btn btn-primary" href="#" role="button">Trade</button>
+            </div>
         </div>;
     }
 }

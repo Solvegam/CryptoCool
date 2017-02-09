4e09221595bbd09214c5dca9e00485237df9b8e7
slentsov <s.u.lentsov@gmail.com>
2017-02-09 18:39:43 +0200

    Watson test

--- src/main/java/com/crypto/cool/resources/MainResource.java
+++ src/main/java/com/crypto/cool/resources/MainResource.java
@@ -1,9 +1,15 @@
 package com.crypto.cool.resources;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
+
+import com.ibm.watson.developer_cloud.alchemy.v1.AlchemyDataNews;
+import com.ibm.watson.developer_cloud.alchemy.v1.model.DocumentsResult;
 
 @Path("mainresource")
 public class MainResource {
@@ -13,4 +19,26 @@ public class MainResource {
 	public String getIt(){
 		return "Got it!";
 	}
+
+	@GET
+	@Path("watson-test")
+	@Produces(MediaType.APPLICATION_JSON)
+	public DocumentsResult watsonDo(){
+		AlchemyDataNews service = new AlchemyDataNews();
+		service.setApiKey("d816d0de9724e4f9d1588afaf56f601c9dfd0268");
+
+		Map<String, Object> params = new HashMap<String, Object>();
+
+		params.put(AlchemyDataNews.RETURN, "enriched.url.title,enriched.url.url,enriched.url.author,enriched.url.publicationDate,enriched.url.enrichedTitle.entities,enriched.url.enrichedTitle.docSentiment");
+		params.put(AlchemyDataNews.START, "1440720000");
+		params.put(AlchemyDataNews.END, "1441407600");
+		params.put(AlchemyDataNews.COUNT, 7);
+
+		DocumentsResult result = service.getNewsDocuments(params).execute();
+
+		System.out.println(result);
+		return result;
+	}
+
+
 }
